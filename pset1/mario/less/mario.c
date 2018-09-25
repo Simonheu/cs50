6a2676703e504f537f4a3b42eb79b6cfacc7081a
Simon Heuschkel <simon.heuschkel@code.berlin>
2018-09-25 19:55:07 +0000

    First attempt

---
+++ pset1/mario/less/mario.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int zahl;
+
+    do {
+        zahl = get_int("num: ");
+    } while(zahl < 0 || zahl > 23);
+    printf("hello\n");
+}
+
+
+
+
+    /*
+    n = height
+
+
+
+    for (int i = 0; i < height; i++)
+    {
+        printf(" ")
+        printf("##")
+        print(/n)
+
+
+    }*/
